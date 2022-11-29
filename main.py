0501229b2ff2c28717e7c120ab162cbfe700db09
Yuy-1 <DM-Yuy@proton.me>
2022-11-28 19:09:27 -0500

    add @app.get('/products') import and add middleware.cors

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection, HashModel
 
 app = FastAPI()
+
+add.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*']
+    allow_headers=['*']
+)
 
 redis = get_redis_connection(
     host="redis-17048.c57.us-east-1-4.ec2.cloud.redislabs.com:17048"
@@ -18,3 +26,6 @@ class Product(HashModel):
     class Meta:
         database = redis
 
+@app.get('/products')
+def all():
+    return Product.all_pks()
